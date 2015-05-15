640142cf4bddf28c229ca9e22a28da8c8a7985fb
RosmaryFC <Rosmaryfc@hotmail.com>
2015-05-15 13:15:28 -0400

    Initial commit, basic calc layout

---
+++ scientificCalculator/ScientificCalculator/app/src/main/java/nyc/c4q/rosmaryfc/scientificcalculator/MainActivity.java
@@ -0,0 +1,50 @@
+package nyc.c4q.rosmaryfc.scientificcalculator;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EditText editEquation = (EditText) findViewById(R.id.equation);
+
+
+        //how to add a text to another view by pressing a button and assigning a character to it.
+
+        Button buttons = (Button) findViewById(R.id.one);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
